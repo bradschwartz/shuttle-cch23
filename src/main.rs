742d75d61ba2634538fb24438f6d30cf78da909b
bradschwartz <baschwartz95@gmail.com>
2023-11-27 11:55:33 -0800

    Add forced error 500 route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use rocket::{get, routes};
+use rocket::{get, routes, http::Status};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/-1/error")]
+fn forced_500() -> Status {
+    Status::InternalServerError
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, forced_500]);
 
     Ok(rocket.into())
 }
