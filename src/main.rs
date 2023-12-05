db66260de2a0a4fbfa052349f8c83e3e9d88a286
bradschwartz <baschwartz95@gmail.com>
2023-12-04 19:35:11 -0800

    day4 task1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use rocket::routes;
 
 mod day1;
+mod day4;
 mod warmup;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount(
         "/",
-        routes![warmup::index, warmup::forced_500, day1::cube_bits],
+        routes![
+            warmup::index,
+            warmup::forced_500,
+            day1::cube_bits,
+            day4::strength,
+        ],
     );
 
     Ok(rocket.into())
