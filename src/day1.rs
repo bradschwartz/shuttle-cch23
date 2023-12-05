1a25d9b16ba6fab4cfdf6af454484425c1317d5b
bradschwartz <baschwartz95@gmail.com>
2023-12-04 19:09:21 -0800

    day1 task2, replaces task1 as generic solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,32 @@
+use std::path::PathBuf;
+
 use rocket::get;
 
-#[get("/1/<num1>/<num2>")]
-pub fn cube_bits(num1: i64, num2: i64) -> String {
-    let xor = num1 ^ num2;
+#[get("/1/<num_parts..>")]
+pub fn cube_bits(num_parts: PathBuf) -> String {
+    let mut xor: i64 = 0;
+    num_parts.iter().for_each(|part| {
+        xor ^= part.to_str().unwrap().parse::<i64>().unwrap();
+    });
     let powed = xor.pow(3);
     print!("{}", powed);
     powed.to_string()
 }
 
 #[test]
-fn problem_example() {
-    let result = cube_bits(4, 8);
+fn task1_problem_example() {
+    let num_parts = PathBuf::from("4/8");
+    let result = cube_bits(num_parts);
     assert_eq!(result, "1728")
+}
+
+#[test]
+fn task2_problem_example() {
+    let num_parts = PathBuf::from("10");
+    let result = cube_bits(num_parts);
+    assert_eq!(result, "1000");
+
+    let num_parts = PathBuf::from("4/5/8/10");
+    let result = cube_bits(num_parts);
+    assert_eq!(result, "27");
 }
